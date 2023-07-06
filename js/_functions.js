fcf504be87d6555e9b01500d63756ca772539ee5
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-07-06 22:57:17 +0300

    сверстал блоки blog и events

--- js/_functions.js
+++ js/_functions.js
@@ -1,9 +1,10 @@
 import { burger } from "./functions/burger.js";
 
-const swiper = new Swiper(".events__swiper", {
+const eventsSlider = new Swiper(".events__swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  slidesPerView: 1,
   spaceBetween: 40,
 
   breakpoints: {
@@ -17,7 +18,7 @@ const swiper = new Swiper(".events__swiper", {
       spaceBetween: 30,
     },
 
-    1024: {
+    1200: {
       slidesPerView: 3,
       spaceBetween: 40,
     },
@@ -27,5 +28,27 @@ const swiper = new Swiper(".events__swiper", {
   navigation: {
     nextEl: ".events__button_next",
     prevEl: ".events__button_prev",
+  },
+});
+
+const blogSlider = new Swiper(".blog__swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+
+  navigation: {
+    nextEl: ".blog__button_next",
+    prevEl: ".blog__button_prev",
   },
 });
