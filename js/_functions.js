8e1ebbbc0e345fa35d662475c727af64890fd374
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-07-06 16:52:33 +0300

    сверстал блок событий

--- js/_functions.js
+++ js/_functions.js
@@ -1,1 +1,31 @@
 import { burger } from "./functions/burger.js";
+
+const swiper = new Swiper(".events__swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 40,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".events__button_next",
+    prevEl: ".events__button_prev",
+  },
+});
