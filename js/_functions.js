a8a52ff8a5d9061765df13b34faff27694ac8bb8
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-07-07 13:37:47 +0300

    добавил кастомные селекты

--- js/_functions.js
+++ js/_functions.js
@@ -146,3 +146,26 @@ function init() {
 }
 
 ymaps.ready(init);
+
+let select = function (id) {
+  const select = document.getElementById(id);
+  const selectCurrent = select.querySelector(".select__current");
+  const selectHeader = select.querySelector(".select__header");
+  const selectItem = select.querySelectorAll(".select__item");
+
+  selectHeader.addEventListener("click", function () {
+    select.classList.toggle("select_active");
+  });
+
+  selectItem.forEach((item) => {
+    item.addEventListener("click", function () {
+      const text = item.textContent;
+      selectCurrent.textContent = text;
+      selectCurrent.classList.remove("select__current_empty");
+      select.classList.remove("select_active");
+    });
+  });
+};
+
+select("spot-select");
+select("city-select");
