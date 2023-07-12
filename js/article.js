b48c3c8ff35f73ce5dc86b3975019981d37ab31e
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-07-12 17:26:44 +0300

    сделал страницу статьи

---
+++ js/article.js
@@ -0,0 +1,62 @@
+import "./_vendor.js";
+import vars from "./_vars.js";
+
+import { burger } from "./functions/burger.js";
+
+const blogSlider = new Swiper(".blog__swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+
+  navigation: {
+    nextEl: ".blog__button_next",
+    prevEl: ".blog__button_prev",
+  },
+});
+
+const eventsSlider = new Swiper(".events__swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".events__button_next",
+    prevEl: ".events__button_prev",
+  },
+});
