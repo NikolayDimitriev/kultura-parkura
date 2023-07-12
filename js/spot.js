b9460a555919a6ba73fd311921c41237c789d287
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-07-12 20:54:06 +0300

    сделал модальное окно

--- js/spot.js
+++ js/spot.js
@@ -2,3 +2,25 @@ import "./_vendor.js";
 import vars from "./_vars.js";
 
 import { burger } from "./functions/burger.js";
+
+const openModalBtn = document.querySelector(".add-spot__button");
+const modal = document.getElementById("modal");
+
+openModalBtn.addEventListener("click", function () {
+  modal.classList.add("modal_active");
+});
+
+modal.addEventListener("click", function (e) {
+  const target = e.target;
+  const condition =
+    target.classList.contains("modal__close") ||
+    target.classList.contains("modal__icon");
+
+  if (condition) {
+    modal.classList.remove("modal_active");
+  }
+
+  if (target.classList.contains("modal_active")) {
+    modal.classList.remove("modal_active");
+  }
+});
